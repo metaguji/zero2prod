735842f1c3b38ebfa2bc6b9d6c3081f66b4f9613
metaguji <59725664+metaguji@users.noreply.github.com>
2024-10-24 19:25:02 +0800

    Add integration test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse, Responder};
+use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(hello_world))
-            .route("/{name}", web::get().to(hello_world))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
-
-async fn hello_world(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!\n", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    run(listener)?.await
 }
